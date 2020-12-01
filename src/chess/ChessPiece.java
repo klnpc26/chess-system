3e2f70e0a7e5d5941f30280750848e27110843a5
kaique <kaique@gmail.com>
2020-12-01 01:21:52 -0300

    Special move - En Passant

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,37 +4,38 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
-public abstract class ChessPiece extends Piece{
-    
-    private Piece piece;
-    private Color color;
-    private int moveCount;
- 
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-    }
+public abstract class ChessPiece extends Piece {
 
-    public Color getColor() {
-        return color;
-    }
-    
-    public int getMoveCount(){
-        return moveCount;
-    }
-    
-    public void increaseMoveCount(){
-        moveCount++;
-    }
-    
-    public void decreaseMoveCount(){
-        moveCount--;
-    }
-    public ChessPosition getChessPosition(){
-        return ChessPosition.fromPosition(position);
-    }
-    protected  boolean isThereOpponentPiece(Position position){
-         ChessPiece p = (ChessPiece)getBoard().piece(position);
-         return p != null && p.getColor() != color;
-    }
+	private Color color;
+	private int moveCount;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
